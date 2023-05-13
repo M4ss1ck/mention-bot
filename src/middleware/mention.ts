993ffeb7649f37f8175860904c5f802a51c2bb3f
M4ss1ck <34517932+M4ss1ck@users.noreply.github.com>
2023-05-13 19:50:34 -0400

    fix /all command

--- src/middleware/mention.ts
+++ src/middleware/mention.ts
@@ -1,25 +1,36 @@
 import { Composer } from "tgsnake"
+import { logger } from "../logger/winston.js"
 
 export const mention = new Composer()
 
 mention.command('all', async ctx => {
-    await ctx.reply('calling everybody...')
-    if (ctx.chat.id) {
-        const users = await ctx.telegram.getChatMembers(ctx.chat.id)
-        if (users) {
-            const toMention = users.participants.map(u => u.user).filter(u => !u.self && !u.deleted && !u.bot)
-            console.log(toMention)
-            const msgAmount = Math.ceil(toMention.length / 3)
-            for (let i = 0; i < msgAmount; i++) {
-                let msg = '<i>Calling...</i> '
-                let user1 = toMention[1 + i * 3]
-                let user2 = toMention[2 + i * 3]
-                let user3 = toMention[3 + i * 3]
-                if (user1) msg += ` <a href="tg://user?id=${user1.id.toString()}">${user1.firstName ? user1.firstName : user1.id.toString()}</a>`
-                if (user2) msg += ` <a href="tg://user?id=${user2.id.toString()}">${user2.firstName ? user2.firstName : user2.id.toString()}</a>`
-                if (user3) msg += ` <a href="tg://user?id=${user3.id.toString()}">${user3.firstName ? user3.firstName : user3.id.toString()}</a>`
-                ctx.replyWithHTML(msg)
+    try {
+        if (ctx.chat.id && ctx.from.id) {
+            const { status } = await ctx.telegram.getParticipant(ctx.chat.id, ctx.from.id.toString())
+            if (status === 'creator' || status === 'admin') {
+                await ctx.reply('calling everybody...')
+                const users = await ctx.telegram.getChatMembers(ctx.chat.id)
+                if (users) {
+                    const replyTo = ctx.replyToMessage ? ctx.replyToMessage.id : ctx.id
+                    const toMention = users.participants.map(u => u.user).filter(u => !u.self && !u.deleted && !u.bot)
+                    const msgAmount = Math.ceil(toMention.length / 3)
+                    for (let i = 0; i < msgAmount; i++) {
+                        let msg = '<i>Calling...</i> '
+                        let user1 = toMention[i * 3]
+                        let user2 = toMention[1 + i * 3]
+                        let user3 = toMention[2 + i * 3]
+                        logger.debug([user1, user2, user3])
+                        if (user1) msg += ` <a href="tg://user?id=${user1.id.toString()}">${user1.firstName ? user1.firstName : user1.id.toString()}</a>`
+                        if (user2) msg += ` <a href="tg://user?id=${user2.id.toString()}">${user2.firstName ? user2.firstName : user2.id.toString()}</a>`
+                        if (user3) msg += ` <a href="tg://user?id=${user3.id.toString()}">${user3.firstName ? user3.firstName : user3.id.toString()}</a>`
+                        ctx.replyWithHTML(msg, {
+                            replyToMsgId: replyTo,
+                        })
+                    }
+                }
             }
         }
+    } catch (error) {
+        logger.error(error)
     }
 })
